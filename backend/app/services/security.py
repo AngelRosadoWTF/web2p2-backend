e2ab83e0eccd1adeec6f272da1c365ad7d1799b3
AngelRosadoWTF <angsoft05@gmail.com>
2026-05-14 17:05:09 -0600

    Commit_5
    corrección datetime

--- backend/app/services/security.py
+++ backend/app/services/security.py
@@ -13,8 +13,8 @@ def pwdVerify(plain_password: str, hashed_password: str):
 
 def crearToken(data: dict):
     to_encode = data.copy()
-    expire = datetime + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    expire = datetime.now() + timedelta(
+        minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES)
     )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
